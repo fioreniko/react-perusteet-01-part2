d2ac8cf361af5c18e49c2a4e4b2f80155d986744
Fiorella Dalla Riva <s2400314@edu.bc.fi>
2024-09-25 13:29:14 +0300

    Destrukturointi

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import Hello from "./components/Hello";
+const App = () => {
+  const nimi = "Pekka";
+  const ika = 10;
+
+  return (
+    <div>
+      <h1>Greetings</h1>
+      <Hello name="Maya" age={26 + 10} />
+      <Hello name={nimi} age={ika} />
+    </div>
+  );
+};
+export default App;
---
+++ src/components/Hello.jsx
@@ -0,0 +1,19 @@
+const Hello = ({ name, age }) => {
+  const bornYear = () => new Date().getFullYear() - age;
+  /*
+    const bornYear = () => {
+  return new Date().getFullYear() - age
+    }
+*/
+
+  return (
+    <div>
+      <p>
+        Hello {name}, you are {age} years old
+      </p>
+      <p>So you were probably born {bornYear()}</p>
+    </div>
+  );
+};
+
+export default Hello;
