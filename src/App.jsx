8f2343a0fd83511c2b4b70350e662f015e8de680
Fiorella Dalla Riva <s2400314@edu.bc.fi>
2024-10-20 16:52:37 +0300

    Tilallinen komponentti

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,14 @@
 //https://fullstackopen.com/osa1/komponentin_tila_ja_tapahtumankasittely
+//Tilallinen komponentti
 
-const App = (props) => {
-  const { counter } = props;
+import { useState } from "react";
+
+const App = () => {
+  const [counter, setCounter] = useState(0);
+
+  setTimeout(() => setCounter(counter + 1), 1000);
+  console.log("rendering...", counter);
+
   return <div>{counter}</div>;
 };
 
--- src/main.jsx
+++ src/main.jsx
@@ -1,21 +1,6 @@
 //https://fullstackopen.com/osa1/komponentin_tila_ja_tapahtumankasittely
-
 import ReactDOM from "react-dom/client";
 
 import App from "./App";
 
-let counter = 1;
-
-const refresh = () => {
-  ReactDOM.createRoot(document.getElementById("root")).render(<App counter={counter} />);
-};
-
-refresh();
-counter += 1;
-refresh();
-counter += 1;
-refresh();
-setInterval(() => {
-  refresh();
-  counter += 1;
-}, 1000);
+ReactDOM.createRoot(document.getElementById("root")).render(<App />);
