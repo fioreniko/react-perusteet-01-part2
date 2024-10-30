2878c76b4fa2343f5c34988e5f6d7dba6272c406
Fiorella Dalla Riva <s2400314@edu.bc.fi>
2024-10-30 09:59:20 +0200

    Tilan vieminen alikomponenttiin

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,25 @@
 //https://fullstackopen.com/osa1/komponentin_tila_ja_tapahtumankasittely
-//Tapahtumankäsittelijä on funktio
+//Tilan vieminen alikomponenttiin
 
 import { useState } from "react";
+import Button from "./components/Button";
+import Display from "./components/Display";
 
 const App = () => {
   const [counter, setCounter] = useState(0);
 
   const increaseByOne = () => setCounter(counter + 1);
-
+  const decreaseByOne = () => setCounter(counter - 1);
   const setToZero = () => setCounter(0);
 
   return (
     <div>
-      <div>{counter}</div>
-      <button onClick={increaseByOne}>plus</button>
-      <button onClick={setToZero}>zero</button>
+      <Display counter={counter} />
+      <Button onClick={increaseByOne} text="plus" />
+      <Button onClick={setToZero} text="zero" />
+      <Button onClick={decreaseByOne} text="minus" />
     </div>
   );
 };
+
 export default App;
