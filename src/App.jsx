af58fade2920705aca624e4cb8372d89c4d487a2
Fiorella Dalla Riva <s2400314@edu.bc.fi>
2024-11-04 11:04:17 +0200

    Tapahtumankäsittely uudelleen

--- src/App.jsx
+++ src/App.jsx
@@ -1,38 +1,19 @@
 //https://fullstackopen.com/osa1/komponentin_tila_ja_tapahtumankasittely
-//React-sovellusten debuggaus
+//Tapahtumankäsittely uudelleen
 
 import { useState } from "react";
-import Button from "./components/Button";
-import Display from "./components/Display";
-import History from "./components/History";
-
 const App = () => {
-  const [left, setLeft] = useState(0);
-  const [right, setRight] = useState(0);
-  const [allClicks, setAll] = useState([]);
-  const [total, setTotal] = useState(0);
+  const [value, setValue] = useState(10);
 
-  const handleLeftClick = () => {
-    setAll(allClicks.concat("L"));
-    const updatedLeft = left + 1;
-    setLeft(updatedLeft);
-    setTotal(updatedLeft + right);
-  };
-
-  const handleRightClick = () => {
-    setAll(allClicks.concat("R"));
-    const updatedRight = right + 1;
-    setRight(updatedRight);
-    setTotal(left + updatedRight);
+  const handleClick = () => {
+    console.log("clicked the button");
+    setValue(0);
   };
 
   return (
     <div>
-      {left}
-      <Button handleClick={handleLeftClick} text="left" />
-      <Button handleClick={handleRightClick} text="right" />
-      {right}
-      <History allClicks={allClicks} />
+      {value}
+      <button onClick={handleClick}>button</button>
     </div>
   );
 };
